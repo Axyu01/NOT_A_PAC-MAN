484ad278b0d5ec5d0c64407602eb25f53ccac6ee
Dualithe <dualithe@gmail.com>
2023-06-21 19:35:17 +0200

    Create duplicate of test scene, create border and getborder method

--- NOT_A_PAC-MAN/Assets/_Scripts/MapGen.cs
+++ NOT_A_PAC-MAN/Assets/_Scripts/MapGen.cs
@@ -7,7 +7,8 @@ public class MapGen : MonoBehaviour
 {
     [SerializeField] public GameObject[] objects;
     [SerializeField] public TextToMapParser parser;
-
+    private Vector2 borderMaxXY = Vector2.zero;
+    private Vector2 borderMinXY = Vector2.zero;
     void Start()
     {
         //parser = GetComponent<TextToMapParser>();
@@ -16,6 +17,7 @@ public class MapGen : MonoBehaviour
 
     void CreateMap(int[][] map)
     {
+        int[][] mapDuplicate = map;
         for (int y = 0; y < map.Length; y++)
         {
             for (int x = 0; x < map[y].Length; x++)
@@ -24,5 +26,18 @@ public class MapGen : MonoBehaviour
             }
         }
         GameObject.FindGameObjectWithTag("MainCamera").GetComponent<mapSizePositionAdjuster>().changeCam(map.Length, map[1].Length);
+        setBorderPosition(mapDuplicate);
+    }
+
+    private void setBorderPosition(int[][] map)
+    {
+        borderMaxXY.x = map[0].Length - 0.5f;
+        borderMinXY.y = -map.Length + 0.5f;
+        borderMaxXY.y = 0.5f;
+        borderMinXY.x = -0.5f;
+    }
+    public Vector4 getBorderPosition()
+    {
+        return new Vector4(borderMinXY.x, borderMinXY.y, borderMinXY.x, borderMinXY.y);
     }
 }
